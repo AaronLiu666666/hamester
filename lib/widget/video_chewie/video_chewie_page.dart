7a7aeb6b881c61d14a1991f38a93523097ffe45d
liurong <liur9@chinatelecom.cn>
2024-05-01 18:18:52 +0800

    2024-05-01 删除无用代码

--- lib/widget/video_chewie/video_chewie_page.dart
+++ lib/widget/video_chewie/video_chewie_page.dart
@@ -9,115 +9,6 @@ import 'package:video_player/video_player.dart';
 
 import 'custom_material_controls.dart';
 
-
-// class VideoChewiePage extends StatelessWidget {
-//   final int videoId;
-//   final String videoPath;
-//   final int? seekTo;
-//
-//   VideoChewiePage({required this.videoId, required this.videoPath, this.seekTo});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetBuilder<VideoChewiePageController>(
-//       init: VideoChewiePageController(),
-//       builder: (controller) {
-//         return FutureBuilder<Widget>(
-//           future: controller.buildVideoWidget(videoPath, videoId, seekTo),
-//           builder: (context, snapshot) {
-//             if (snapshot.connectionState == ConnectionState.waiting) {
-//               return Center(child: CircularProgressIndicator());
-//             } else {
-//               return snapshot.data ?? Container();
-//             }
-//           },
-//         );
-//       },
-//     );
-//   }
-// }
-//
-// class VideoChewiePageController extends GetxController {
-//   late VideoPlayerController _videoPlayerController;
-//   late ChewieController _chewieController;
-//   late VideoHorizontalScrollPagingController _videoHorizontalScrollPagingController;
-//
-//   late int _videoId;
-//   late String _videoPath;
-//   late int? _seekTo;
-//
-//   void setVideoInfo({required int videoId, required String videoPath, int? seekTo}) {
-//     _videoId = videoId;
-//     _videoPath = videoPath;
-//     _seekTo = seekTo;
-//   }
-//
-//   Future<Widget> buildVideoWidget(String videoPath, int videoId, int? seekTo) async {
-//     try {
-//       await initialize(videoPath: videoPath, videoId: videoId, seekTo: seekTo);
-//       return Chewie(
-//         controller: _chewieController,
-//       );
-//     } catch (e) {
-//       print("Error building video widget: $e");
-//       return Container(); // Return an empty container if initialization fails
-//     }
-//   }
-//
-//   Future<void> initialize({required String videoPath, required int videoId, int? seekTo}) async {
-//     print("Initializing VideoPlayerController for $videoPath");
-//     _videoPlayerController = VideoPlayerController.file(File(videoPath));
-//
-//     await _videoPlayerController.initialize();
-//     print("VideoPlayerController initialized successfully.");
-//
-//     _chewieController = ChewieController(
-//       videoPlayerController: _videoPlayerController,
-//       autoPlay: true,
-//       looping: false,
-//       showControls: true,
-//       customControls: CustomMaterialControls(videoId: videoId, seekTo: seekTo),
-//       aspectRatio: _videoPlayerController.value.aspectRatio,
-//     );
-//
-//     _videoHorizontalScrollPagingController = Get.put(VideoHorizontalScrollPagingController());
-//     // 如果加了这个update 会导致死循环initialize
-//     // update();
-//   }
-//
-//   @override
-//   void onClose() {
-//     _videoPlayerController.dispose();
-//     _chewieController.dispose();
-//     _videoHorizontalScrollPagingController.dispose();
-//     Get.delete<VideoHorizontalScrollPagingController>();
-//     super.onClose();
-//   }
-//
-//   void switchVideo({required int videoId, required String videoPath, int? seekTo}) async {
-//     await _videoPlayerController.pause();
-//     await _videoPlayerController.dispose();
-//     _chewieController.dispose();
-//     // 更新界面状态
-//     update();
-//     print("Initializing VideoPlayerController for $videoPath");
-//     _videoPlayerController = VideoPlayerController.file(File(videoPath));
-//
-//     await _videoPlayerController.initialize();
-//     print("VideoPlayerController initialized successfully.");
-//
-//     _chewieController = ChewieController(
-//       videoPlayerController: _videoPlayerController,
-//       autoPlay: true,
-//       looping: false,
-//       showControls: true,
-//       customControls: CustomMaterialControls(videoId: videoId,seekTo: seekTo),
-//       aspectRatio: _videoPlayerController.value.aspectRatio,
-//     );
-//     // update();
-//   }
-// }
-
 class VideoChewiePageController extends GetxController {
   late VideoPlayerController _videoPlayerController;
   late ChewieController _chewieController;
@@ -162,8 +53,6 @@ class VideoChewiePageController extends GetxController {
     );
 
     _videoHorizontalScrollPagingController = Get.put(VideoHorizontalScrollPagingController());
-    // 如果加了这个update 会导致死循环initialize
-    // update();
   }
 
   @override
