14fb0a7ac6583bc5eff3ae3a6a8cf5950f988749
liurong <liur9@chinatelecom.cn>
2024-05-04 16:48:49 +0800

    2024-05-04 删除无用代码

--- lib/widget/video_chewie/video_chewie_page.dart
+++ lib/widget/video_chewie/video_chewie_page.dart
@@ -85,22 +85,8 @@ class VideoChewiePageController extends GetxController {
     super.onInit();
     _videoHorizontalScrollPagingController =
         Get.put(VideoHorizontalScrollPagingController());
-    // videoHorizontalScrollWidget = VideoHorizontalScrollWidget(key: const ValueKey<String>("dfjalsdkf"),);
     initialize();
   }
-
-  // void setVideoInfo(
-  //     {required int videoId,
-  //     required String videoPath,
-  //     int? seekTo,
-  //     VideoPageFromType videoPageFromType = VideoPageFromType.media_page,
-  //     String? tagId}) {
-  //   _videoId.value = videoId;
-  //   _videoPath.value = videoPath;
-  //   _seekTo.value = seekTo ?? 0;
-  //   _videoPageFromType = videoPageFromType;
-  //   _tagId = tagId;
-  // }
 
   VideoPageFromType getVideoPageFromType() {
     return _videoPageFromType;
@@ -147,8 +133,6 @@ class VideoChewiePageController extends GetxController {
           Timer.periodic(const Duration(milliseconds: 3000), (timer) {
         videoHorizontalScrollWidgetHideStuff.value = true;
       });
-      // _videoHorizontalScrollPagingController =
-      //     Get.put(VideoHorizontalScrollPagingController());
     } finally {
       isIniting.value = false;
     }
@@ -195,12 +179,6 @@ class VideoChewiePageController extends GetxController {
       looping: false,
       showControls: true,
       autoInitialize: true,
-      // customControls:
-      // CustomMaterialControls(
-      //     videoHorizontalScrollWidget:
-      //         VideoChewiePageController.videoHorizontalScrollWidget,
-      //     videoId: _videoId.value,
-      //     seekTo: _seekTo.value),
       customControls: CustomMaterialControls(
         videoId: _videoId.value,
         seekTo: _seekTo.value,
@@ -232,98 +210,6 @@ class VideoChewiePage extends GetView<VideoChewiePageController> {
   Widget build(BuildContext context) {
     VideoChewiePageController controller =
         Get.find<VideoChewiePageController>();
-    // https://github.com/fluttercommunity/chewie/issues/742 查找chewie issues 将横向滚动切换条放到父组件中
-    // return Scaffold(
-    //     backgroundColor: Colors.black,
-    //     body: SafeArea(
-    //         child: Stack(children: [
-    //       Center(
-    //         child: Container(
-    //           height: MediaQuery.of(context).size.width * 9 / 16,
-    //           child: Obx(() {
-    //             return controller.isIniting.value == false
-    //                 ? Chewie(
-    //                     controller: controller.getChewieController(),
-    //                   )
-    //                 : Center(child: CircularProgressIndicator());
-    //           }),
-    //         ),
-    //       ),
-    //       Positioned(
-    //         bottom: 0,
-    //         left: 0,
-    //         right: 0,
-    //         child: Obx(() {
-    //           // 在不显示的时候吸收点击事件，否则就算隐藏了，还是能够进行点击
-    //           // 但是这样还是有个问题，下面拖动框正拖动着呢，拖着拖着自己隐藏了。。。
-    //           return AbsorbPointer(
-    //             absorbing:
-    //                 controller.videoHorizontalScrollWidgetHideStuff.value,
-    //             // child: AnimatedOpacity(
-    //             //   opacity: controller.videoHorizontalScrollWidgetHideStuff.value ? 0 : 1,
-    //             //   duration: const Duration(milliseconds: 300),
-    //             //   child: Container(
-    //             //     height: 100,
-    //             //     padding: EdgeInsets.all(2),
-    //             //     width: double.maxFinite,
-    //             //     child: controller.videoHorizontalScrollWidget,
-    //             //   ),
-    //             // ),
-    //             child: GestureDetector(
-    //               behavior: HitTestBehavior.opaque,
-    //               onTap: () {
-    //                 // Handle tap event
-    //                 controller.resetVideoHorizontalScrollWidgetHideStuffTimer();
-    //                 controller.videoHorizontalScrollWidgetHideStuff.value =
-    //                     false;
-    //               },
-    //               onVerticalDragStart: (details) {
-    //                 // Handle vertical drag start event
-    //                 controller.resetVideoHorizontalScrollWidgetHideStuffTimer();
-    //                 controller.videoHorizontalScrollWidgetHideStuff.value =
-    //                     false;
-    //               },
-    //               onVerticalDragUpdate: (details) {
-    //                 // Handle vertical drag update event
-    //                 controller.resetVideoHorizontalScrollWidgetHideStuffTimer();
-    //                 controller.videoHorizontalScrollWidgetHideStuff.value =
-    //                     false;
-    //               },
-    //               onHorizontalDragStart: (details) {
-    //                 /**
-    //                     https://juejin.cn/post/6844903954959056904?from=search-suggest
-    //                     横向滑动的组件会把滚动事件给拦截，父组件监听不动子组件的滚动，也就没有办法重置videoHorizontalScrollWidgetHideStuff
-    //
-    //                  */
-    //                 // Handle horizontal drag start event
-    //                 controller.resetVideoHorizontalScrollWidgetHideStuffTimer();
-    //                 controller.videoHorizontalScrollWidgetHideStuff.value =
-    //                     false;
-    //               },
-    //               onHorizontalDragUpdate: (details) {
-    //                 // Handle horizontal drag update event
-    //                 controller.resetVideoHorizontalScrollWidgetHideStuffTimer();
-    //                 controller.videoHorizontalScrollWidgetHideStuff.value =
-    //                     false;
-    //               },
-    //               child: AnimatedOpacity(
-    //                 opacity:
-    //                     controller.videoHorizontalScrollWidgetHideStuff.value
-    //                         ? 0
-    //                         : 1,
-    //                 duration: const Duration(milliseconds: 300),
-    //                 child: Container(
-    //                   height: 100,
-    //                   padding: EdgeInsets.all(2),
-    //                   width: double.maxFinite,
-    //                   child: controller.videoHorizontalScrollWidget,
-    //                 ),
-    //               ),
-    //             ),
-    //           );
-    //         }),
-    //       ),
-    //     ])));
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
