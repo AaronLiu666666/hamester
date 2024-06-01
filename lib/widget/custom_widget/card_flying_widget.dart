d1965d2de99b40b2814f83864b4741a3286565e7
liurong <liur9@chinatelecom.cn>
2024-06-01 15:08:43 +0800

    2024-06-01 目前动画卡片点击成功 但是消失出现效budui

--- lib/widget/custom_widget/card_flying_widget.dart
+++ lib/widget/custom_widget/card_flying_widget.dart
@@ -20,7 +20,8 @@ abstract class CardFlyingController<M> extends GetxController
     if (isAnimating) {
       animationController.stop();
     } else {
-      animationController.repeat(reverse: false);
+      // animationController.repeat(reverse: false);
+      animationController.forward();
     }
     isAnimating = !isAnimating;
     update(); // 更新状态
@@ -33,7 +34,8 @@ abstract class CardFlyingController<M> extends GetxController
   }
 
   void startAnimation() {
-    animationController.repeat(reverse: false);
+    // animationController.repeat(reverse: false);
+    animationController.forward();
     isAnimating = true;
     update(); // 更新状态
   }
@@ -56,8 +58,9 @@ abstract class CardFlyingController<M> extends GetxController
       begin: Offset(1.0, 0.0),
       end: Offset(-2.0, 0.0), // 改变动画范围
     ).animate(animationController);
-
-    animationController.repeat(reverse: false);
+    updateCurrentDisplayData();
+    animationController.forward();
+    // animationController.repeat(reverse: false);
     animationController.addStatusListener((status) {
       if (status == AnimationStatus.completed ||
           status == AnimationStatus.dismissed) {
@@ -65,10 +68,10 @@ abstract class CardFlyingController<M> extends GetxController
         animationController.forward(from: 0.0);
       }
     });
-    updateCurrentDisplayData();
   }
 
   void updateCurrentDisplayData() {
+    print("updateCurrentDisplayData");
     cardPositions.clear();
     currentDisplayData.clear();
     for (int i = 0; i < cardNum; i++) {
@@ -126,104 +129,6 @@ abstract class CardFlyingController<M> extends GetxController
     return position;
   }
 }
-
-// Widget buildCardFlyingWidget<T, C extends CardFlyingController<T>>(
-//     {required Widget Function(T item, int index) itemBuilder,
-//     Function(T item, int index)? onItemClick,
-//     Function(T item, int index)? onItemLongPress,
-//     String? tag}) {
-//   C controller = Get.find(tag: tag);
-//   final double maxHeight = MediaQuery.of(Get.context!).size.height -
-//       150; // 150 is the card height to ensure cards stay in view
-//   final double maxWidth = MediaQuery.of(Get.context!).size.width -
-//       100; // 100 is the card width to ensure cards stay in view
-//   return Stack(
-//     children: List.generate(controller.currentDisplayData.length, (index) {
-//       if (controller.cardPositions.length < controller.cardNum) {
-//         controller.cardPositions
-//             .add(controller.getRandomPosition(maxHeight, maxWidth));
-//       }
-//       Offset position = controller.cardPositions[index];
-//       T data = controller.currentDisplayData[index];
-//       return AnimatedBuilder(
-//         animation: controller.animationController,
-//         builder: (context, child) {
-//           double screenWidth = MediaQuery.of(context).size.width;
-//           double cardPositionX =
-//               (controller.animation.value.dx * screenWidth + position.dx) %
-//                       (screenWidth + 100) -
-//                   100;
-//           // return Transform.translate(
-//           //   offset: Offset(cardPositionX, position.dy),
-//           //   child: GestureDetector(
-//           //     /*
-//           //     GestureDetector 的behavior处理事件的方式有以下三种：
-//           //     HitTestBehavior.opaque、 HitTestBehavior.deferToChild、HitTestBehavior.translucent
-//           //
-//           //     HitTestBehavior.opaque 自己处理事件
-//           //     HitTestBehavior.deferToChild child处理事件
-//           //     HitTestBehavior.translucent 自己和child都可以接收事件
-//           //      */
-//           //     behavior: HitTestBehavior.translucent,
-//           //     onTap: (){
-//           //       print("onTapxx");
-//           //       controller.stopAnimation();
-//           //       onItemClick?.call(controller.currentDisplayData[index], index);
-//           //     },
-//           //     onLongPress: () {
-//           //       print("onLongpressxx");
-//           //       controller.stopAnimation();
-//           //       onItemLongPress?.call(controller.currentDisplayData[index], index);
-//           //     },
-//           //     child: itemBuilder(controller.currentDisplayData[index], index),
-//           //     // child:Container(
-//           //     //   width: 160,
-//           //     //   height: 120,
-//           //     //   child: Image.asset('assets/image/no-pictures.png'),
-//           //     // ),
-//           //   ),
-//           // );
-//           /*
-//             Transform可以在其子组件绘制时对其应用一些矩阵变换来实现一些特效
-//             Transform.translate 平移
-//             Transform.rotate 旋转
-//             Transform.scale 缩放
-//
-//            */
-//           return SlideTransition(
-//             position: controller.animation,
-//             child: Transform.translate(
-//               offset: Offset(cardPositionX, position.dy),
-//               child: GestureDetector(
-//                 behavior: HitTestBehavior.translucent,
-//                 // child: InkWell(
-//                 onTap: () {
-//                   print("onTapxx");
-//                   controller.stopAnimation();
-//                   onItemClick?.call(
-//                       controller.currentDisplayData[index], index);
-//                 },
-//                 onLongPress: () {
-//                   print("onLongpressxx");
-//                   controller.stopAnimation();
-//                   onItemLongPress?.call(
-//                       controller.currentDisplayData[index], index);
-//                 },
-//                 child: Container(
-//                   width: 160,
-//                   height: 120,
-//                   color: Colors.white, // 确保点击区域可见
-//                   child:
-//                       itemBuilder(controller.currentDisplayData[index], index),
-//                 ),
-//               ),
-//             ),
-//           );
-//         },
-//       );
-//     }),
-//   );
-// }
 
 Widget buildCardFlyingWidget<T, C extends CardFlyingController<T>>(
     {required Widget Function(T item, int index) itemBuilder,
